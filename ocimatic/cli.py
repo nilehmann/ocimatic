eec226417476991e35443d1bfb8f11075aacc65f
Nico Lehmann <nlehmann@dcc.uchile.cl>
2015-09-26 16:11:13 -0300

    usage

--- ocimatic/cli.py
+++ ocimatic/cli.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8; -*-
 import os
 import sys
-# import getopt
+import getopt
 import textwrap
 import re
 from .contest import Contest, create_layout_for_contest
@@ -22,14 +22,34 @@ def colorize(text, color):
     return color + text + RESET
 
 
+def bold(text):
+    return colorize(text, BOLD)
+
+
+def underline(text):
+    return colorize(text, UNDERLINE)
+
+
 def decolorize(text):
     u"Strip ANSI coloring from `text`."
     return re.sub(r'\033\[[0-9]+m', '', text)
+
+
+def writeln(text=''):
+    sys.stdout.write(text+'\n')
 
 INFO = BOLD
 OK = BOLD + GREEN
 WARNING = BOLD + YELLOW
 ERROR = BOLD + RED
+
+
+def error_message(message):
+    writeln('ocimatic: ' + message)
+    writeln('Usage: ' + bold('ocimatic ') + underline('contest|problem') +
+            ' ' + underline('COMMAND') + ' ...')
+    writeln('Try ' + bold('ocimatic -h') + ' for more information.')
+    sys.exit(1)
 
 
 def show_message(label, message, color=INFO):
@@ -41,8 +61,7 @@ def show_message(label, message, color=INFO):
     lines[0] = lines[0][indent_size:]
     sys.stdout.write(' %s ' % colorize(label + ':', color + UNDERLINE))
     sys.stdout.write('\n'.join(lines))
-    print()
-    print()
+    sys.stdout.write('\n')
 
 
 def task_header(name, message):
@@ -82,9 +101,83 @@ def end_task(status=None):
     print()
 
 
+tab_width = 6
+
+
+def indent(level, text):
+    writeln(' '*level*tab_width + text)
+    # writeln('\t'*level + text)
+
+
+def description(level, text, width=90):
+    # line = ''
+    # tab_width = 6
+    lines = textwrap.wrap(text,
+                          width,
+                          initial_indent=' '*tab_width*level,
+                          subsequent_indent=' '*tab_width*level)
+    writeln('\n'.join(lines))
+
+
+def header(text):
+    writeln(bold(text))
+
+
 def usage():
-    print()
-    print("usage:")
+    writeln(bold('Usage: ocimatic ') + underline('[OPTION]') + ' ... ' +
+            underline('contest|problem') +
+            ' ' + underline('COMMAND') + ' ...')
+    writeln()
+
+    header('DESCRIPTION')
+    description(1, 'Ocimatic is a tool for automating tasks related to the'
+                ' creation of problems for the Chilean Olympiad in Informatics'
+                ' (OCI).')
+    writeln()
+    description(1, 'To specify the task to perform you should provide one of'
+                ' the following modes')
+    writeln()
+    indent(1, bold('contest'))
+    description(2, 'Performs contest related tasks.')
+    writeln()
+    indent(1, bold('problem'))
+    description(2, 'Performs tasks related to problems. If ocimatic is called'
+                ' inside a problem directory or subdirectory the task is'
+                ' performed for the specific problem. Otherwise the task is'
+                ' performed for all problems in the contest. For example, '
+                ' calling ' + bold('ocimatic problem check') + ' in the contest'
+                'root will check all problems input/output correctness.')
+    writeln('')
+
+    header('CONTEST COMMANDS')
+    indent(1, bold('new') + ' ' + underline('NAME'))
+    description(2, 'Create a new contest in the current directory with the'
+                ' given name.')
+    indent(1, bold('pdf'))
+    description(2, 'Merge the statements of all problems generating a'
+                ' problemset pdf.')
+    writeln()
+
+    header('PROBLEM COMMANDS')
+    indent(1, bold('new') + ' ' + underline('NAME'))
+    description(2, 'Create a new problem with the given name.')
+    indent(1, bold('expected'))
+    description(2, 'Generates expected output files (*.sol) for all input'
+                ' testdata (*.in) using any correct solution.')
+    indent(1, bold('pdf'))
+    description(2, 'Generates pdf file for the problem statement.')
+    indent(1, bold('check'))
+    description(2, 'Checks input/output running all correct solutions with all'
+                ' testdata and sample inputs, and comparing the results with'
+                ' the excepted output.')
+    indent(1, bold('build'))
+    description(2, 'Build all correct and partial solutions.')
+    writeln()
+
+    header('COMMANDS')
+    indent(1, bold('-h'))
+    description(2, 'Display this help.')
+    writeln()
     sys.exit(1)
 
 
@@ -106,22 +199,23 @@ def change_directory():
             head, tail = os.path.split(head)
         os.chdir('..')
         if os.getcwd() == '/':
-            show_message('ERROR',
-                         'ocimatic was not called inside a contest',
-                         ERROR)
-            usage()
+            error_message('ocimatic was not called inside a contest.')
     return last_dir
 
 
 def parse_arguments():
     """Parse options returning the command and the rest of the arguments."""
-    # optlist, args = getopt.gnu_getopt(sys.argv[1:], 'l:s')
-    args = sys.argv[1:]
     try:
-        cmd = args.pop(0)
+        optlist, args = getopt.getopt(sys.argv[1:], 'h|v')
+    except getopt.GetoptError as err:
+        error_message(str(err))
+
+    try:
+        mode = args.pop(0)
     except IndexError:
         usage()
-    return cmd, args
+
+    return mode, args, optlist
 
 
 def new_contest(args):
@@ -132,9 +226,7 @@ def new_contest(args):
     if status:
         show_message('Info', 'Contest [%s] created' % contest_name)
     else:
-        show_message('Error',
-                     'Couldn\'t create contest',
-                     ERROR)
+        error_message('Couldn\'t create contest')
 
 
 def contest(args):
@@ -202,7 +294,7 @@ def problem(args):
     if not args:
         usage()
 
-    cmds = ['new', 'build', 'check', 'gen-sol', 'pdf']
+    cmds = ['new', 'build', 'check', 'expected', 'pdf']
 
     problem_call = change_directory()
     contest = Contest(os.getcwd())
@@ -221,7 +313,7 @@ def problem(args):
 
         if args[0] == 'build':
             build_problems(problems)
-        elif args[0] == 'gen-sol':
+        elif args[0] == 'expected':
             gen_sol_files(problems)
         elif args[0] == 'check':
             check_problem(problems)
@@ -232,11 +324,16 @@ def problem(args):
 
 
 def main():
-    cmd, args = parse_arguments()
+    mode, args, optlist = parse_arguments()
 
-    if cmd == "contest":
+    if mode not in ['contest', 'problem']:
+        error_message('Unknown mode.')
+
+    for (key, val) in optlist:
+        if key == '-h':
+            usage()
+
+    if mode == "contest":
         contest(args)
-    elif cmd == "problem":
+    elif mode == "problem":
         problem(args)
-    else:
-        usage()
